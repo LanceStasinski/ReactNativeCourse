32bada13696222351f34cdb2b157be8facc01b3d
LanceStasinski <lance.stasinski@gmail.com>
2022-05-04 11:23:09 -0400

    feat: Add event handlers to input and button

--- App.tsx
+++ App.tsx
@@ -1,14 +1,37 @@
-import { StatusBar } from "expo-status-bar";
-import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+import { useState } from "react";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TextInputChangeEventData,
+  NativeSyntheticEvent,
+} from "react-native";
 
 export default function App() {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const goalInputHandler = (
+    enteredText: NativeSyntheticEvent<TextInputChangeEventData>
+  ) => {
+    setEnteredGoalText(enteredText.nativeEvent.text);
+  };
+
+  const addGoalHandler = () => {
+    console.log(enteredGoalText)
+  };
+
   return (
     <View style={styles.appContainer}>
       <View style={styles.inputContainer}>
-        <TextInput style={styles.textInput} placeholder="Your Course Goals" />
-        <Button onPress={() => {}} title="Add Goal" />
+        <TextInput
+          style={styles.textInput}
+          placeholder="Your Course Goals"
+          onChange={goalInputHandler}
+        />
+        <Button onPress={addGoalHandler} title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals</Text>
       </View>
     </View>
@@ -17,17 +40,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
+    flex: 1,
     padding: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
   },
   textInput: {
     borderWidth: 1,
     borderColor: "#cccccc",
-    width: "80%",
+    width: "70%",
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
